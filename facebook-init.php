4af461f255ee0cc0528a1ffb0215b8d310786cac
fulviocanducci <fulviocanducci@hotmail.com>
2021-04-16 13:02:28 -0300

    remove token

--- facebook-init.php
+++ facebook-init.php
@@ -2,7 +2,7 @@
 session_start();
 require 'vendor/autoload.php';
 $fb = new Facebook\Facebook([
-    'app_id' => '500445857190303',
-    'app_secret' => '6d3231bc2156c3488cde5d73e18f13fa',
+    'app_id' => '',
+    'app_secret' => '',
     'default_graph_version' => 'v2.10',
 ]);
